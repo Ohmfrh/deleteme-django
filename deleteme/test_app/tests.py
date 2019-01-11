80ac892ffdda351d0d863f6fd1a43b693a0afcea
Daniel Castro <dancassig@gmail.com>
2019-01-11 15:18:41 -0600

    Removed failing test

--- deleteme/test_app/tests.py
+++ deleteme/test_app/tests.py
@@ -6,5 +6,5 @@ class Test1(unittest.TestCase):
     def test_pass(self):
         self.assertEqual(0, 0)
 
-    def test_fail(self):
-        self.fail('FORCE FAIL')
+    # def test_fail(self):
+    #     self.fail('FORCE FAIL')
