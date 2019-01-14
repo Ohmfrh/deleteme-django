626621ef4e08d067918eec67c21fe5b17a896503
Daniel Castro <dancassig@gmail.com>
2019-01-13 22:24:44 -0600

    Added key path

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-(ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ./deploy/deploy.yml -u ubuntu -i ./hosts)
+(ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ./deploy/deploy.yml -u ubuntu -i ./hosts --private-key=\
+/home/jenkins/.ssh/id_rsa)
